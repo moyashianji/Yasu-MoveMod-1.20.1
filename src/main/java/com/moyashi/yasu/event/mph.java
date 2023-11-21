a1d0b93eda20183796bc3ee3c1869cbab8ecd2c6
moyashianji <136812257+moyashianji@users.noreply.github.com>
2023-11-22 01:33:55 +0900

    9days(syuusei2

--- src/main/java/com/moyashi/yasu/event/mph.java
+++ src/main/java/com/moyashi/yasu/event/mph.java
@@ -4,11 +4,14 @@ import com.moyashi.yasu.init.IroiroModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import static com.moyashi.yasu.item.RocketItem.TARGET_DIMENSION_KEY;
 
 public class mph {
 
@@ -17,6 +20,13 @@ public class mph {
         if (event.getEntity() instanceof Player) {
             Player player = (Player) event.getEntity();
 
+            if(player.level().dimension() == TARGET_DIMENSION_KEY) {
+
+                if (!player.onGround() && !player.isFallFlying()) {
+                    // プレイヤーがジャンプ中ならy座標をゆっくりと下げる
+                    player.setDeltaMovement(player.getDeltaMovement().x, player.getDeltaMovement().y + 0.05, player.getDeltaMovement().z);
+                }
+            }
             System.out.println(player.getMainHandItem().getItem().toString());
             if(player.getMainHandItem().getItem() == IroiroModItems.MPH.get()) {
                 if (player.isInWater()) {
